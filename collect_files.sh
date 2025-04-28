6754bf86731dbbc8afcdae00d31ba714343e7970
Полищук Артем Николаевич <likekiseryota@MacBook-Pro-Polisuk-2.local>
2025-04-28 19:03:24 +0300

    Try 10

--- collect_files.sh
+++ collect_files.sh
@@ -37,19 +37,28 @@ mkdir -p "$output_dir" || {
 copy() {
     local src="$1"
     local relative_path="${src#$input_dir/}"
-    local clean_relative_path=$(echo "$relative_path" | sed 's/ \././g')
-    local path="$output_dir/$clean_relative_path"
-    local app_dir=$(dirname "$path")
+    local clean_relative_path=$(echo "$relative_path" | sed 's/ \././g' | tr -s '/')
+    local dest="$output_dir/$clean_relative_path"
 
-    mkdir -p "$app_dir" || {
-        echo "Ошибка: Не удалось создать директорию '$app_dir'"
-        return 1
-    }
+    if [ -d "$src" ]; then
+        mkdir -p "$dest"
+    else 
+        mkdir -p "$(dirname "$dest")"
+        local filename="$(basename "$dest")"
+        local name="${filename%.*}"
+        local ext_file="${filename##*.}"
+        [ "$name" = "$filename" ] && ext_file="" || ext_file=".$ext_file"
 
-    cp -- "$src" "$path" || {
-        echo "Ошибка: Не получилось скопировать '$src' в '$path'"
-        return 1
-    }
+        local count=1
+        local main_path="$(dirname "$dest")/$name"
+        local fin_path="$main_path$ext_file"
+        while [ -e "$fin_path" ]; do
+            fin_path="${main_path}_${count}${ext_file}"
+            ((count++))
+        done
+
+        cp -- "$src" "$fin_path" || return 1
+    fi
 }
 
 export -f copy
@@ -60,17 +69,13 @@ if [ -n "$max_depth" ]; then
         echo "Ошибка: --max_depth должен быть числом"
         exit 1
     fi
-    find "$input_dir" -mindepth 1 -maxdepth "$max_depth" -type f -print0 | while IFS= read -r -d '' file; do
-        copy "$file"
-    done
-
-    find "$input_dir" -mindepth 1 -maxdepth "$max_depth" -type d -print0 | while IFS= read -r -d '' dir; do
-        relative_path="${dir#$input_dir/}"
-        clean_relative_path=$(echo "$relative_path" | sed 's/ \././g')
-        mkdir -p "$output_dir/$clean_relative_path"
+    find "$input_dir" -mindepth 1 -maxdepth "$max_depth" \( -type f -o -type d \) -print0 | \
+    while IFS= read -r -d '' path; do
+        copy "$path"
     done
 else
-    find "$input_dir" -type f -print0 | while IFS= read -r -d '' file; do
-        copy "$file"
+    find "$input_dir" -mindepth 1 \( -type f -o -type d \) -print0 | \
+    while IFS= read -r -d '' path; do
+        copy "$path"
     done
 fi
